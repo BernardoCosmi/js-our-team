fa3b09ee43a37ba53b24879ff841247fca2c8329
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-01-19 14:52:37 +0100

    bonus

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,5 @@
+const containerHTML = document.getElementById("container");
+
 //ANCHOR Creazione array oggetti
 const teamMembers = [
     {
@@ -38,11 +40,28 @@ teamMembers.forEach((membro)=>{
     console.log(`Nome: ${membro.nome} \nRuolo: ${membro.ruolo} \nFoto: ${membro.foto}`)
 })
 
-//ANCHOR stampa delle info sul DOM
-const containerHTML = document.getElementById("container");
+//NOTE Bonus (stampa delle card)
+teamMembers.forEach((membro) => {
+    const memberDivHTML = document.createElement("div");
+    memberDivHTML.classList.add("card");
 
+    const immagineHTML = document.createElement("img")
+    immagineHTML.src = membro.foto
+    immagineHTML.alt = `${membro.foto}`
+
+    memberDivHTML.appendChild(immagineHTML);
+
+    memberDivHTML.innerHTML += `<p><strong>Nome:</strong> ${membro.nome}</p>`;
+    memberDivHTML.innerHTML += `<p><strong>Ruolo:</strong> ${membro.ruolo}</p>`;
+
+    // Aggiunta della scheda al container
+    containerHTML.appendChild(memberDivHTML);
+})
+
+//ANCHOR stampa delle info in modo brutto sul DOM
 teamMembers.forEach((membro) => {
     const memberDivHTML = document.createElement("div");
     memberDivHTML.innerHTML = `<p class="card">Nome: ${membro.nome} <br> Ruolo: ${membro.ruolo} <br> Foto: ${membro.foto}</p>`;
     containerHTML.appendChild(memberDivHTML);
 });
+
