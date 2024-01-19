ebfd1da27655f306825cf991722d8fd1c9f4dd74
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-01-19 13:58:49 +0100

    file upload

---
+++ assets/main.js
@@ -0,0 +1,34 @@
+//ANCHOR Creazione array oggetti
+const teamMembers = [
+    {
+        nome: "Wayne Barnett"
+        ruolo:"Founder & CEO"
+        foto:"wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        nome: "Angela Caroll"
+        ruolo:"Chief Editor"
+        foto: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        nome: ""
+        ruolo:""
+        foto:""
+    },
+    {
+        nome: ""
+        ruolo:""
+        foto:""
+    },
+    {
+        nome: ""
+        ruolo:""
+        foto:""
+    },
+    {
+        nome: ""
+        ruolo:""
+        foto:""
+    },
+];
+console.log(teamMembers);
